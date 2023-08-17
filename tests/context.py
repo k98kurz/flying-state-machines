33606c15b82b9db510ac13c7c40fc6e80a172282
Jonathan Voss <k98kurz@gmail.com>
2023-08-17 06:42:58 -0400

    fix for namespace change

--- tests/context.py
+++ tests/context.py
@@ -2,5 +2,5 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-import pyfsm
-from pyfsm import classes
+import flying_state_machines
+from flying_state_machines import classes
